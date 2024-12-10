d6a5fc688ac4b4aafffe8ce7eb3a86b7e974b7a1
janusz.janowski <januszjanowski@outlook.com>
2024-12-10 23:42:28 +0100

    reformat with Prettier add workaround on 401 in refresh token

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,58 +1,75 @@
-import {Navigate} from "react-router-dom"
+import { Navigate } from "react-router-dom";
 
-import {jwtDecode} from "jwt-decode"
-import api from "../api"
+import { jwtDecode } from "jwt-decode";
+import api from "../api";
 
-import { REFRESH_TOKEN, ACCESS_TOKEN } from "../constants"
-import { useState, useEffect } from "react"
+import { REFRESH_TOKEN, ACCESS_TOKEN } from "../constants";
+import { useState, useEffect } from "react";
 
-function ProtectedRoute({children}) {
-    const [isAuthorized, setIsAuthorized] = useState(null)
+function ProtectedRoute({ children }) {
+  const [isAuthorized, setIsAuthorized] = useState(null);
 
-    useEffect(()=>{
-        auth().catch(()=> setIsAuthorized(false))
-    })
+  useEffect(() => {
+    auth().catch(() => setIsAuthorized(false));
+  });
 
-    const refreshToken = async() => {
-        const refreshToken = localStorage.getItem(REFRESH_TOKEN)
-        try {
-            const res = await api.post("api/token/refresh/",{
-                refresh: refreshToken,
-            })
-            if (res.status == 200){
-                localStorage.setItem(ACCESS_TOKEN, res.data.access)
-                setIsAuthorized(true)
-            } else {
-                setIsAuthorized(false)
-            }
-        } catch (error) {
-            console.log(error)
-            setIsAuthorized(false)
+  const refreshToken = async () => {
+    const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+    try {
+      const res = await api.post(
+        "api/token/refresh/",
+        {
+          refresh: refreshToken,
+        },
+        {
+          validateStatus: (status) => {
+            console.log(status);
+            return status == 200 || status == 401;
+          },
         }
+      );
+      switch (res.status) {
+        case 200:
+          localStorage.setItem(ACCESS_TOKEN, res.data.access);
+          setIsAuthorized(true);
+          break;
+        case 401:
+          // If get 401 on refresh token that means is not usable for example expired od remove from server side
+          localStorage.removeItem(ACCESS_TOKEN);
+          localStorage.removeItem(REFRESH_TOKEN);
+          setIsAuthorized(false);
+          break;
+        default:
+          setIsAuthorized(false);
+      }
+    } catch (error) {
+      console.log(error);
+      setIsAuthorized(false);
     }
-    
-    const auth = async() => {
-        const token = localStorage.getItem(ACCESS_TOKEN)
-        if (!token) {
-            setIsAuthorized(false)
-            return
-        }
-        const decode = jwtDecode(token)
-        const tokenExpiration = decode.exp
-        const now = Date.now() / 1000
+  };
 
-        if (tokenExpiration < now) {
-            await refreshToken()
-        } else {
-            setIsAuthorized(true)
-        }
+  const auth = async () => {
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    if (!token) {
+      setIsAuthorized(false);
+      return;
     }
+    const decode = jwtDecode(token);
+    const tokenExpiration = decode.exp;
+    const now = Date.now() / 1000;
 
-    if (isAuthorized === null) {
-        return <div>Loading...</div>
+    if (tokenExpiration < now) {
+      await refreshToken();
+    } else {
+      setIsAuthorized(true);
     }
+  };
 
-    return isAuthorized ? children : <Navigate to="/login"/>
+  if (isAuthorized === null) {
+    return <div>Loading...</div>;
+  }
+
+  return isAuthorized ? children : <Navigate to="/login" />;
 }
 
-export default ProtectedRoute
+export default ProtectedRoute;
