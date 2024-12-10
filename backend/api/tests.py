36c94e28b785d7acdae0a43f31c699df8f8948ad
janusz.janowski <januszjanowski@outlook.com>
2024-12-11 00:11:34 +0100

    add simple test checking if workflow working

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
-
+from django.test import TestCase, client
+from django.urls import reverse
+from django.contrib.auth.models import User
 # Create your tests here.
+
+class TestUser(TestCase):
+    def test_register_user(self):
+        res = self.client.post(reverse('register'), {'username': 'abc', 'password': 'cde'})
+        assert res.status_code == 201
+        user = User.objects.get_by_natural_key('abc')
+        assert user.check_password('cde')
